d1e695983e1a204a3561fba447dcd6d7ef038543
michaellennox <michaelctlennox@gmail.com>
2015-12-07 17:30:28 +0000

    Added form to create name

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 
 get '/random-cat' do
-  @name_sample = ["Amigo", "Oscar", "Viking"].sample
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
 get '/named-cat' do
   p params
-  @name_sample = params[:name]
+  @name = params[:name]
   erb(:index)
 end
