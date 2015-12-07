e708e954b1ed319f8fef8f697354195e0ea05c7c
Richard Moss <richard@englishtutorslive.com>
2015-12-07 16:50:32 +0000

    adds h1

--- app.rb
+++ app.rb
@@ -3,15 +3,3 @@ require 'sinatra'
 get '/' do
   erb(:index)
 end
-
-get '/secret' do
-  "I've got the key, I've got the secret"
-end
-
-get '/randomshit' do
-  "<img src='https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg'></img>"
-end
-
-get '/cat' do
-  "<img src='http://bit.ly/1eze8aE' style='border: 1px dashed red'></img>"
-end
